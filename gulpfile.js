cc6e80e5ed2c104e3b7228a2ea6d11de9946db30
baclap <clapperbrett@gmail.com>
2015-11-21 09:27:25 -0800

    Update stylesheet compiling to be prepended with css reset

--- gulpfile.js
+++ gulpfile.js
@@ -12,29 +12,32 @@ const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 const minifyCss = require('gulp-minify-css');
 const less = require('gulp-less');
+const streamqueue = require('streamqueue');
 
 // this not only starts the app but will also monitor for file changes and
 // restart the app when changes are detected
 gulp.task('nodemon', function() {
-  nodemon({
-    script: 'app.js',
-    nodeArgs: ['--harmony']
-  }).on('restart');
+    nodemon({
+        script: 'app.js',
+        nodeArgs: ['--harmony']
+    }).on('restart');
 });
 
 // run mocha test when changes detected in files
 gulp.task('watch', function() {
-  //concat css
-  gulp.watch(
-    ['assets/less/**/*.less'], ['bundle-css']
-  )
+    //concat css
+    gulp.watch(
+        ['assets/less/**/*.less'], ['bundle-css']
+    )
 });
 
 gulp.task('bundle-css', function(){
-  return gulp.src(['assets/less/**/*.less', '!assets/less/mixins/*.less'])
-    .pipe(less({
-      paths: ['assets/less/mixins']
-    }))
+    return streamqueue({ objectMode: true },
+        gulp.src(['assets/css/reset.css']),
+        gulp.src(['assets/less/**/*.less', '!assets/less/mixins/*.less']).pipe(less({
+            paths: ['assets/less/mixins']
+        }))
+    )
     .pipe(concat('bundle.css'))
     .pipe(gulp.dest('public/css'))
     .pipe(rename('bundle.min.css'))
@@ -45,49 +48,49 @@ gulp.task('bundle-css', function(){
 // one-off browserify task which is handy when debugging
 // node --harmony `which gulp` browserify
 gulp.task('browserify', function() {
-  const b = getBrowserifyInstance();
-  b.transform(babelify);
-  return bundleBrowserify(b);
+    const b = getBrowserifyInstance();
+    b.transform(babelify);
+    return bundleBrowserify(b);
 })
 
 // update bundle.js when changes detected in client-side js/jsx
 gulp.task('watchify', function() {
-  // create watchify instance wrapping our browserify instance
-  // re-run compile whenever watchify emits an update event
-  const b = getBrowserifyInstance();
-  const w = watchify(b);
+    // create watchify instance wrapping our browserify instance
+    // re-run compile whenever watchify emits an update event
+    const b = getBrowserifyInstance();
+    const w = watchify(b);
 
-  w.transform(babelify);
-  w.on('update', function() {
-    console.log('updating bundle');
+    w.transform(babelify);
+    w.on('update', function() {
+        console.log('updating bundle');
+        bundleBrowserify(w);
+    });
     bundleBrowserify(w);
-  });
-  bundleBrowserify(w);
 });
 
 const getBrowserifyInstance = function() {
-  // create browserify instance
-  const b = browserify('assets/js/app.jsx', {
-    debug: true,
-    extensions: ['.jsx'],
+    // create browserify instance
+    const b = browserify('assets/js/app.jsx', {
+        debug: true,
+        extensions: ['.jsx'],
 
-    // watchify args
-    cache: {},
-    packageCache: {}
-  });
+        // watchify args
+        cache: {},
+        packageCache: {}
+    });
 
-  return b;
+    return b;
 }
 
 // receives a browserify instance and bundles it
 const bundleBrowserify = function(b) {
-  return b
+    return b
     .bundle(function(err){
-      if(err){
-        console.log(err.message);
-      } else {
-        console.log('bundle done');
-      }
+        if(err){
+            console.log(err.message);
+        } else {
+            console.log('bundle done');
+        }
     })
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('public/js'))
