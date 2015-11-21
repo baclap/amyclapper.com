4f9ffb6c820796f5cf033f5c7055f7b7aa2ed694
baclap <clapperbrett@gmail.com>
2015-11-21 09:31:42 -0800

    Unstyled homepage squares with empty popups

--- app.js
+++ app.js
@@ -7,5 +7,8 @@ const serve = require('koa-static');
 const app = koa();
 app.use(logger());
 app.use(serve('public'));
+app.use(function *(next) {
+    this.redirect('/');
+});
 app.listen(3333);
 console.log('Listening on 3333...');
--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -2,94 +2,47 @@
 
 import { React, Base } from './components/base'
 import { render, findDOMNode } from 'react-dom'
-import { Router, Route, Link } from 'react-router'
+import { Router, Route, IndexRoute, Link } from 'react-router'
+import createBrowserHistory from 'history/lib/createBrowserHistory'
 import classnames from 'classnames'
 
-class Menu extends Base {
+class Main extends Base {
     render() {
         return (
-            <ul className="menu">
+            <div>
+                <nav className="home-blocks">
+                    <Link className="block" to="/section">Section</Link>
+                    <Link className="block" to="/section">Section</Link>
+                    <Link className="block" to="/section">Section</Link>
+                    <Link className="block" to="/section">Section</Link>
+                    <Link className="block" to="/section">Section</Link>
+                    <Link className="block" to="/section">Section</Link>
+                </nav>
                 {this.props.children}
-            </ul>
-        )
-    }
-}
-
-class MenuItem extends Base {
-    render() {
-        return (
-            <li className={classnames([
-                    'menu-item',
-                    {'active': this.props.to == this.props.currentPath}
-                ])}>
-                <Link to={this.props.to}>{this.props.children}</Link>
-            </li>
-        )
-    }
-}
-
-class App extends Base {
-    render() {
-        return (
-            <div className="app-body">
-                <Menu>
-                    <MenuItem to="/example-a" currentPath={this.props.location.pathname}>
-                        Example A
-                    </MenuItem>
-                    <MenuItem to="/example-b" currentPath={this.props.location.pathname}>
-                        Example B
-                    </MenuItem>
-                </Menu>
-                <p>
-                    {this.props.children}
-                </p>
             </div>
         )
     }
 }
 
-class ExampleA extends Base {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 1
-        }
-    }
-    componentDidMount() {
-        console.log('A is mounting...')
-        const link = findDOMNode(this.refs.link);
-        $(link).button()
-    }
-    componentWillUnmount() {
-        console.log('A is unmounting...')
-    }
-    handleClick(e) {
-        e.preventDefault();
-        let newCount = this.state.count + 1;
-        this.setState({
-            count: newCount
-        })
-    }
+class Popup extends Base {
     render() {
         return (
-            <a href="#" ref="link" onClick={this.handleClick}>{this.state.count}</a>
-        )
-    }
-}
-
-class ExampleB extends Base {
-    render() {
-        return (
-            <span>this is b</span>
+            <div>
+                <div className="backdrop" />
+                <div onClick={this.props.history.goBack} className="popup-container">
+                    <div className="popup">
+                        {this.props.children}
+                    </div>
+                </div>
+            </div>
         )
     }
 }
 
 render((
-    <Router>
-        <Route path="/" component={App}>
-            <Route path="example-a" component={ExampleA} />
-            <Route path="example-b" component={ExampleB} />
+    <Router history={createBrowserHistory()}>
+        <Route path="/" component={Main}>
+            <Route path="section" component={Popup} />
         </Route>
     </Router>
 ), document.querySelector('#app'))
