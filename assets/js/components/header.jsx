df53a93481c30508550d4bc304645d3ba4fc36cc
baclap <clapperbrett@gmail.com>
2015-11-21 23:51:53 -0800

    Header styling

--- assets/js/components/header.jsx
+++ assets/js/components/header.jsx
@@ -1,25 +1,37 @@
 'use strict';
 
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 
 export default class Header extends Component {
     render() {
         return (
             <header className="header">
-                <span className="logo">Amy Clapper</span>
+                <span className="logo">
+                    <Link to="/">AmyClapper</Link>
+                </span>
                 <nav className="nav">
                     <ul>
-                        <li className="link">
+                        <li>
                             <a href="#">About Me</a>
                         </li>
-                        <li className="link">
+                        <li className="divider">|</li>
+                        <li>
                             <a href="#">Contact Info</a>
                         </li>
-                        <li className="link">
+                        <li className="divider">|</li>
+                        <li>
                             <a href="#">Download Portfolio</a>
                         </li>
                     </ul>
                 </nav>
+                <div className="hamburger-wrap">
+                    <div className="hamburger">
+                        <div />
+                        <div />
+                        <div />
+                    </div>
+                </div>
             </header>
         )
     }
